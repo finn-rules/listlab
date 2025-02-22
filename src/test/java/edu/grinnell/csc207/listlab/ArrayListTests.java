46a3fedaa414da54d30773b708dbba5b0d883e1d
finn-rules <finn.rowles@gmail.com>
2025-02-21 22:08:43 -0600

    Implement additonal tests in ArrayListTests.java

--- src/test/java/edu/grinnell/csc207/listlab/ArrayListTests.java
+++ src/test/java/edu/grinnell/csc207/listlab/ArrayListTests.java
@@ -9,6 +9,7 @@ import net.jqwik.api.Property;
 import net.jqwik.api.constraints.IntRange;
 
 public class ArrayListTests {
+  
     @Test
     public void emptyListTest() {
         ArrayList list = new ArrayList();
@@ -24,6 +25,29 @@ public class ArrayListTests {
         assertEquals(10, list.size());
     }
 
+    @Test
+    public void removeFromEnd() {
+        ArrayList list = new ArrayList();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        assertEquals(9, list.remove(9));
+        assertEquals(9, list.size());
+    }
+
+    @Test
+    public void removeFromMiddle() {
+        ArrayList list = new ArrayList();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        // int[] lst1 = {0,1,2,3,4,5,7,8,9};
+        // int[] lst2 = {0,1,2,3,4,6,7,8,9};
+        list.remove(6);
+        assertEquals(7, list.get(6));
+        assertEquals(9, list.size());
+    }
+
     @Property
     public boolean listAddSize(@ForAll @IntRange(min = 0, max = 1000) int sz) {
         ArrayList list = new ArrayList();
@@ -31,5 +55,24 @@ public class ArrayListTests {
             list.add(i);
         }
         return list.size() == sz;
+    }
+
+    @Property
+    public boolean listCheckLastElt(@ForAll @IntRange(min = 1, max = 1000) int sz) {
+        ArrayList list = new ArrayList();
+        for (int i = 0; i < sz; i++) {
+            list.add(i);
+        }
+        return list.get(sz - 1) == sz - 1;
+    }
+
+    @Property
+    public boolean listDeleteMiddle(@ForAll @IntRange(min = 1, max = 1000) int sz) {
+        ArrayList list = new ArrayList();
+        for (int i = 0; i < sz; i++) {
+            list.add(i);
+        }
+        list.remove(sz / 2);
+        return list.size() == sz - 1;
     }
 }
